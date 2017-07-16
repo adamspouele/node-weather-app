9dd68f73353e4490ede33c5c66247b31214888d1
adams <adamspouele@gmail.com>
2017-07-16 03:05:52 +0200

    project files

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ Next instructions explain how to get one :
 1 - get a key by signing up here: https://darksky.net/dev/
 
 2 - write your key in the key variable which is in those 2 files :
+    
     * app-promise.js
     * weather/weather.js
 
 Now you can get your weather datas by taping your address with this command:
+    
     node app.js -a "[your address]"
 
     or
---
+++ weather/weather.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+  // get a key here : https://darksky.net/dev/
+  var key = 'write key here';
+  request({
+    url: `https://api.forecast.io/forecast/${key}/${lat},${lng}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to Forecast.io server.');
+    } else if (response.statusCode === 400) {
+      callback('Unable to fetch weather.');
+    } else if (response.statusCode === 200) {
+      callback(undefined, {
+        temperature: body.currently.temperature,
+        apparentTemperature: body.currently.apparentTemperature
+      });
+    }
+  });
+};
+
+module.exports.getWeather = getWeather;
